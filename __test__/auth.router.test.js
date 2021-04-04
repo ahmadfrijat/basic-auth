5a1b5679aa1d394ed71458742bdc06b3de45400b
ahmad alfrijat <75928390+ahmadfrijat@users.noreply.github.com>
2021-04-04 23:24:43 +0300

    Update auth.router.test.js

--- __test__/auth.router.test.js
+++ __test__/auth.router.test.js
@@ -3,56 +3,36 @@ const { server } = require('../src/server.js');
 const supergoose = require('@code-fellows/supergoose');
 // const superTest = require('supertest');
 const request = supergoose(server);
-let id;
+const base64 = require("base-64");
 
 
 
-describe('api server', () => {
 
-    
-    
-      it('should create a new User on POST /signup ', async () => {
-        const obj = {
-            username: 'test',
-            password: 'test',
-          };
-        const response = await request.post('/signup').send(obj);
-        expect(response.status).toEqual(200);
-        expect(response.body.username).toEqual('test');
-        id = response.body._id;
+ 
+    describe('POST /signup create new user', () => {
+      test('Should create new user and return record', async () => {
+        let response = await request
+          .post('/signup').send({ username: 'ahmad', password: '12345' });
+        expect(response.body.username).toEqual('ahmad');
+        expect(response.status).toEqual(201);
       });
-      it('should the user login on POST /signin', async () => {
-        // const obj = {
-        //     username: 'test',
-        //     password: 'test',
-        //   };
-        // const response = await request.post('/signin').send(obj);
-        // expect(response.status).toEqual(200);
-        // expect(response.body.username).toEqual('test');
-        // id = response.body._id;
-		// const response = await request.post('/signup').send({
-		// 	username: 'ahmad',
-		// 	password: '12345',
-		// });
-
-		// const { username, password } = response.body;
-
-		// const response2 = await request
-		// 	.post('/signin')
-		// 	.set(
-
-		// 	);
-
-		// expect(response2.status).toEqual(200);
-	});
-
-	it('handle invalid routes', async () => {
-		const response = await request.get(`/foo`);
-		expect(response.status).toEqual(404);
-	});
-
-	it('handle server errors', async () => {
-		const response = await request.put('/bad');
-		expect(response.status).toEqual(500);
-	});
+      test('POST to /signin to login an existing user', async () => {
+        let user = base64.encode(`ahmad:12345`);
+        let response = await request
+          .post('/signin').set(`Authorization`, `Basic ${user}`);
+        expect(response.body.user.username).toEqual('ahmad');
+        expect(response.status).toEqual(200);
+      });
+    });
+    
+    describe('Not found errors', () => {
+      it('handle invalid routes', async () => {
+        const response = await request.get(`/foo`);
+        expect(response.status).toEqual(404);
+      });
+    
+      it('handle server errors', async () => {
+        const response = await request.put('/bad');
+        expect(response.status).toEqual(500);
+      });
     });
